9334cf7434af07a2c97b85909265400b296b20d5
douglasvinicio <douglas.vinicio@hotmail.com>
2021-01-19 01:05:46 -0600

    Car and CarOwner Classes added

---
+++ Users_Cars_Management_System/Users_Cars_Management_System/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users_Cars_Management_System
+{
+   public class Car
+    {
+        //add the columns
+        public int CarId { get; set; }
+
+        [Required]
+        public string MakeModel { get; set; }
+        [Required]
+        public int OwnerId { get; set; }
+
+        public Car(int carId, string makeModel, int ownerId)
+        {
+            this.CarId = carId;
+            this.MakeModel = makeModel;
+            this.OwnerId = ownerId;
+        }
+        public Car()
+        {
+        }
+    }
+}
--- Users_Cars_Management_System/Users_Cars_Management_System/UsersCarsDbContext.cs
+++ Users_Cars_Management_System/Users_Cars_Management_System/UsersCarsDbContext.cs
@@ -9,7 +9,7 @@ namespace Users_Cars_Management_System
 {
     class UsersCarsDbContext : DbContext
     {
-       // public DbSet<Car> TodoList { get; set; }
-       // public DbSet<CarOwner> TodoList { get; set; }
+       public DbSet<Car> Cars { get; set; }
+       public DbSet<CarOwner> CarOwners { get; set; }
     }
 }
