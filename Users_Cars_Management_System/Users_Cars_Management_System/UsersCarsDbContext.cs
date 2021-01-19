00f77dfbc0f46b535fe376284b456ff043aa4b3e
douglasvinicio <douglas.vinicio@hotmail.com>
2021-01-18 19:44:17 -0600

    EntityFrmework Package Installed / DbContext class Created (code still as comment since the Car and CarOwner Class are not ready yet

---
+++ Users_Cars_Management_System/Users_Cars_Management_System/UsersCarsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users_Cars_Management_System
+{
+    class UsersCarsDbContext : DbContext
+    {
+       // public DbSet<Car> TodoList { get; set; }
+       // public DbSet<CarOwner> TodoList { get; set; }
+    }
+}
